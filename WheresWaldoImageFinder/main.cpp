4860a222a7a418680db8e782b8482c6132b6cddb
DannyGotTea <danielgallagher500@gmail.com>
2016-04-26 06:57:08 +0100

    Image loaded

--- WheresWaldoImageFinder/main.cpp
+++ WheresWaldoImageFinder/main.cpp
@@ -5,12 +5,19 @@
 //  Created by Daniel Gallagher on 26/04/2016.
 //  Copyright (c) 2016 Developer Co. All rights reserved.
 //
+//  He is Wally where I come from but most say Waldo so...
+
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
+using namespace cv;
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
+    std::string imgFile = "/Users/dannyg/Desktop/WheresWaldoImageFinder/Resources/Wheres-Waldo.jpg";
+    
+    Mat img = imread(imgFile, CV_LOAD_IMAGE_COLOR);
+    
+    
 }
