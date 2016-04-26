55a726792e5f196248822c25d10cae6eef845168
DannyGotTea <danielgallagher500@gmail.com>
2016-04-26 06:10:05 +0100

    Initial Commit

---
+++ WheresWaldoImageFinder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WheresWaldoImageFinder
+//
+//  Created by Daniel Gallagher on 26/04/2016.
+//  Copyright (c) 2016 Developer Co. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
