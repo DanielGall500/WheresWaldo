e0d18c2b7db9939d697f26afb619e7e98bdeb1fc
DannyGotTea <danielgallagher500@gmail.com>
2016-04-27 20:13:10 +0100

    Template Matching Integrated

--- WheresWaldoImageFinder/main.cpp
+++ WheresWaldoImageFinder/main.cpp
@@ -13,27 +13,29 @@
 
 using namespace cv;
 
+std::string imgFile = "/Users/dannyg/Desktop/WheresWaldoImageFinder/Resources/Wheres-Waldo.jpg";
+std::string imgName = "Original", resultName = "Result";
+
+std::string templFile =
+"/Users/dannyg/Desktop/WheresWaldoImageFinder/Resources/WheresWaldoBasicTemp.jpg";
+
+Mat img, gry_img, templ, gry_templ, result;
+
+int match_method, max_Trackbar = 5;
+
+void MatchingMethod(int, void*);
+
 int main(int argc, const char * argv[]) {
     
-    std::string imgFile = "/Users/dannyg/Desktop/WheresWaldoImageFinder/Resources/Wheres-Waldo.jpg";
-    std::string templFile = ""; //GET TEMPLATE
-    
-    std::string imgName = "Original", resultName = "Result";
-    
-    Mat img, templ, result;
-    
-    int match_method;
-    int max_Trackbar = 5;
-    
-    img = imread(imgFile);
-    
-    templ = imread(templFile);
+    img = imread(imgFile, 1);
+    templ = imread(templFile, 1);
     
     namedWindow(imgName, CV_WINDOW_AUTOSIZE);
     namedWindow(resultName, CV_WINDOW_AUTOSIZE);
     
     std::string trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED\n 4: TM COEFF \n 5: TM COEFF NORMED";
 
+    //When a change is detected, MatchingMethod is called
     createTrackbar(trackbar_label, imgName, &match_method, max_Trackbar, MatchingMethod);
     
     MatchingMethod(0,0);
@@ -41,3 +43,64 @@ int main(int argc, const char * argv[]) {
     waitKey();
     return 0;
 }
+
+void MatchingMethod(int, void*) {
+    Mat img_display;
+    
+    img.copyTo(img_display);
+    
+    //Stores the matching template location results, creating result matrix
+    int result_cols = img.cols - templ.cols + 1;
+    int result_rows = img.rows - templ.rows + 1;
+    
+    result.create(result_rows, result_cols, CV_32FC1);
+    
+    matchTemplate(img, templ, result, match_method);
+    normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
+    
+    double minVal, maxVal;
+    Point minLoc, maxLoc;
+    Point matchLoc;
+    
+    //Localize minimum and maximum values in the result matrix
+    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+    
+    if(match_method == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED)
+    { matchLoc = minLoc; }
+    else { matchLoc = maxLoc; }
+    
+    rectangle(img_display, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows),
+              Scalar::all(0), 2, 8, 0);
+    
+    rectangle(result, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
+    
+    imshow(imgName, img_display);
+    imshow(resultName, result);
+    
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
