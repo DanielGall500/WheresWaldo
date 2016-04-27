65cbf204759c5071a22803c871dc3c6d91644574
DannyGotTea <danielgallagher500@gmail.com>
2016-04-27 06:52:07 +0100

    Template matching half implemented

--- WheresWaldoImageFinder/main.cpp
+++ WheresWaldoImageFinder/main.cpp
@@ -16,8 +16,28 @@ using namespace cv;
 int main(int argc, const char * argv[]) {
     
     std::string imgFile = "/Users/dannyg/Desktop/WheresWaldoImageFinder/Resources/Wheres-Waldo.jpg";
+    std::string templFile = ""; //GET TEMPLATE
     
-    Mat img = imread(imgFile, CV_LOAD_IMAGE_COLOR);
+    std::string imgName = "Original", resultName = "Result";
     
+    Mat img, templ, result;
     
+    int match_method;
+    int max_Trackbar = 5;
+    
+    img = imread(imgFile);
+    
+    templ = imread(templFile);
+    
+    namedWindow(imgName, CV_WINDOW_AUTOSIZE);
+    namedWindow(resultName, CV_WINDOW_AUTOSIZE);
+    
+    std::string trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED\n 4: TM COEFF \n 5: TM COEFF NORMED";
+
+    createTrackbar(trackbar_label, imgName, &match_method, max_Trackbar, MatchingMethod);
+    
+    MatchingMethod(0,0);
+    
+    waitKey();
+    return 0;
 }
