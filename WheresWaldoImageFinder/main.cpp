e4fa3aa27ae3720659738150ec3bbad91e695053
DannyGotTea <danielgallagher500@gmail.com>
2016-05-18 17:46:45 +0100

    template canny edges found

--- WheresWaldoImageFinder/main.cpp
+++ WheresWaldoImageFinder/main.cpp
@@ -13,83 +13,49 @@
 
 using namespace cv;
 
-std::string imgFile = "/Users/dannyg/Desktop/WheresWaldoImageFinder/Resources/Wheres-Waldo.jpg";
+std::string imgFile = "/Users/dannyg/Desktop/Projects/WheresWaldoImageFinder/Resources/Images/WW_Easy4.jpg";
 std::string imgName = "Original", resultName = "Result";
 
 std::string templFile =
-"/Users/dannyg/Desktop/WheresWaldoImageFinder/Resources/WheresWaldoBasicTemp.jpg";
+"/Users/dannyg/Desktop/WheresWaldoImageFinder/Resources/Templates/WheresWallyTemplateScreenshot.png";
 
 Mat img, gry_img, templ, gry_templ, result;
 
+Mat templ_edges;
+
 int match_method, max_Trackbar = 5;
+int lowThreshold;
+int ratio = 3;
+int kernel_size = 3;
 
 void MatchingMethod(int, void*);
+Mat applyCannyEdge(Mat &img);
 
 int main(int argc, const char * argv[]) {
     
-    img = imread(imgFile, 1);
-    templ = imread(templFile, 1);
+    img = imread(imgFile);
+    templ = imread(templFile);
     
-    namedWindow(imgName, CV_WINDOW_AUTOSIZE);
-    namedWindow(resultName, CV_WINDOW_AUTOSIZE);
-    
-    std::string trackbar_label = "Method: \n 0: SQDIFF \n 1: SQDIFF NORMED \n 2: TM CCORR \n 3: TM CCORR NORMED\n 4: TM COEFF \n 5: TM COEFF NORMED";
-
-    //When a change is detected, MatchingMethod is called
-    createTrackbar(trackbar_label, imgName, &match_method, max_Trackbar, MatchingMethod);
-    
-    MatchingMethod(0,0);
+    templ_edges = applyCannyEdge(templ);
     
     waitKey();
     return 0;
 }
 
 void MatchingMethod(int, void*) {
-    Mat img_display;
     
-    img.copyTo(img_display);
-    
-    //Stores the matching template location results, creating result matrix
-    int result_cols = img.cols - templ.cols + 1;
-    int result_rows = img.rows - templ.rows + 1;
-    
-    result.create(result_rows, result_cols, CV_32FC1);
-    
-    matchTemplate(img, templ, result, match_method);
-    normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
-    
-    double minVal, maxVal;
-    Point minLoc, maxLoc;
-    Point matchLoc;
-    
-    //Localize minimum and maximum values in the result matrix
-    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
-    
-    if(match_method == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED)
-    { matchLoc = minLoc; }
-    else { matchLoc = maxLoc; }
-    
-    rectangle(img_display, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows),
-              Scalar::all(0), 2, 8, 0);
-    
-    rectangle(result, matchLoc, Point(matchLoc.x + templ.cols, matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
-    
-    imshow(imgName, img_display);
-    imshow(resultName, result);
-    
-    return;
 }
 
-
-
-
-
-
-
-
-
-
-
+Mat applyCannyEdge(Mat &img)
+{
+    Mat detected_edges;
+    
+    blur(img, img, Size(kernel_size, kernel_size)); //Reduce noise
+    
+    Canny(img, detected_edges, lowThreshold, lowThreshold * ratio, kernel_size);
+    
+    return detected_edges;
+}
 
 
 
